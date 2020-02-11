d29483dba5ec7078c03f4f97bf104c929cef6eb6
Yuri-Tiofilo <yusilva12345@gmail.com>
2020-02-11 07:41:19 -0300

    Create Modules

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 // import { ToastContainer } from 'react-toastify';
-// import './config/ReactotronConfig';
+import './config/ReactotronConfig';
 import GlobalStyle from './styles/global';
 import Routes from './routes';
 
 import Header from './components/Header';
-// import store from './store';
+import store from './store';
 // import history from './services/history';
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <Header />
-      <Routes />
-    </BrowserRouter>
-    // <Provider store={store}>
-    //   <Router history={history}>
-    // {/* <GlobalStyle /> */}
-    //     <ToastContainer autoClose={3000} />
-    // {/* <Header /> */}
-    //     {/* <Routes /> */}
-    //   </Router>
-    // </Provider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Header />
+        <Routes />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
